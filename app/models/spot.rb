f93b0694b83b793213271914d8b9888235848347
Hugh Morrison <hmorri32@gmail.com>
2018-01-03 16:46:58 -0700

    deletes pseudocode

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,3 +1,7 @@
 class Spot < ApplicationRecord
-
+  # def self.create_spit_reports
+  #   self.where.not(spitcast_id: nil).map do |spot|
+  #     SpitCastService.new(spot[:spitcast_id]).weekly_forecast
+  #   end.flatten
+  # end
 end
