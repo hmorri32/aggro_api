e205238d2d0bb9842eccf7e9f2057ef7a72573ea
Hugh Morrison <hmorri32@gmail.com>
2018-01-03 16:47:06 -0700

    adds spitcast namespace

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      namespace :spitcast do
+        get '/daily/:spitcast_id',  to: 'forecast#daily_forecast'
+        get '/weekly/:spitcast_id', to: 'forecast#weekly_forecast'
+        get '/daily/tides',         to: 'forecast#daily_tides'
+        get '/weekly/tides',        to: 'forecast#weekly_tides'
+      end
       get '/spots', to: "spots#index"
     end
   end
