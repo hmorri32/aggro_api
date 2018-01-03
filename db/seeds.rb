d1d78336750a3f48b18dc0cee58acc74872d4a03
Hugh Morrison <hmorri32@gmail.com>
2018-01-03 15:24:01 -0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,694 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+spots = [
+  {
+    lon: -120.07331365850411,
+    lat: 34.460727859027109,
+    surfline_id: 4991,
+    msw_id: 2625,
+    spitcast_id: 620,
+    name: 'Refugio'
+  },
+  {
+    lon: -119.8821432693011,
+    lat: 34.409079140926913,
+    surfline_id: 4994,
+    msw_id: 268,
+    spitcast_id: 182,
+    name: 'Sands'
+  },
+  {
+    lon: -119.8777636727983,
+    lat: 34.4060390017938,
+    surfline_id: 4995,
+    msw_id: 4251,
+    spitcast_id: 181,
+    name: 'Coal Oil Point/Devereux'
+  },
+  {
+    lon: -119.8425963704844,
+    lat: 34.40487888923785,
+    surfline_id: 4997,
+    msw_id: 269,
+    spitcast_id: 179,
+    name: 'Campus Point'
+  },
+  {
+    lon: -119.7020898699724,
+    lat: 34.397968880785839,
+    surfline_id: 145_542,
+    msw_id: 270,
+    spitcast_id: 177,
+    name: 'Leadbetter'
+  },
+  {
+    lon: 34.404613,
+    lat: -119.687402,
+    surfline_id: 4998,
+    msw_id: 2629,
+    name: 'Sandspit'
+  },
+  {
+    lon: 34.416038,
+    lat: -119.636549,
+    surfline_id: 4999,
+    msw_id: 271,
+    name: 'Hammonds'
+  },
+  {
+    lon: 34.391859,
+    lat: -119.525699,
+    surfline_id: 5001,
+    msw_id: 2701,
+    name: 'Carpinteria/Tarpits'
+  },
+  {
+    lon: -119.4779929046033,
+    lat: 34.37256542974805,
+    surfline_id: 4197,
+    msw_id: 272,
+    spitcast_id: 198,
+    name: 'Rincon'
+  },
+  {
+    lon: 34.355095,
+    lat: -119.442182,
+    surfline_id: 4985,
+    msw_id: 2672,
+    name: 'Mussel Shoals/Little Rincon'
+  },
+  {
+    lon: 34.316750,
+    lat: -119.389240,
+    surfline_id: 4981,
+    msw_id: 274,
+    name: 'Pitas Point'
+  },
+  {
+    lon: -119.3773753582565,
+    lat: 34.319176709938617,
+    surfline_id: 49_737,
+    spitcast_id: 193,
+    name: 'Mondos'
+  },
+  {
+    lon: 34.309334,
+    lat: -119.358505,
+    surfline_id: 4989,
+    msw_id: 2644,
+    name: 'Solimar'
+  },
+  {
+    lon: -119.3398784210307,
+    lat: 34.292337487494912,
+    surfline_id: 4980,
+    msw_id: 2646,
+    spitcast_id: 191,
+    name: 'Emma Wood'
+  },
+  {
+    lon: -119.3001508388765,
+    lat: 34.273069869304877,
+    surfline_id: 4200,
+    msw_id: 275,
+    spitcast_id: 190,
+    name: 'C Street'
+  },
+  {
+    lon: 34.244158,
+    lat: -119.269073,
+    surfline_id: 4201,
+    msw_id: 2632,
+    name: 'Ventura Harbor'
+  },
+  {
+    lon: 34.171836,
+    lat: -119.237817,
+    surfline_id: 4968,
+    msw_id: 276,
+    name: 'Oxnard'
+  },
+  {
+    lat: 34.051,
+    lon: -118.964,
+    surfline_id: 4203,
+    msw_id: 277,
+    spitcast_id: 207,
+    name: 'County Line',
+  },
+  {
+    lat: 34.043762,
+    lon: -118.932943,
+    surfline_id: 4953,
+    msw_id: 2642,
+    name: 'Leo Carillo',
+  },
+  {
+    lat: 34.041956,
+    lon: -118.91593,
+    msw_id: 2643,
+    name: 'Zeros',
+  },
+  {
+    lat: 34.01515759618465,
+    lon: -118.8236361547595,
+    surfline_id: 4949,
+    msw_id: 853,
+    spitcast_id: 206,
+    name: 'Zuma Beach',
+  },
+  {
+    lat: 34.0016731994772,
+    lon: -118.804558744648,
+    surfline_id: 4947,
+    msw_id: 2610,
+    name: 'Point Dume',
+  },
+  {
+    lat: 34.033415,
+    lon: -118.677652,
+    surfline_id: 4209,
+    msw_id: 4204,
+    spitcast_id: 205,
+    name: 'Malibu',
+  },
+  {
+    lat: 34.038649057421,
+    lon: -118.5759544372,
+    surfline_id: 4210,
+    msw_id: 279,
+    spitcast_id: 388,
+    name: 'Topanga',
+  },
+  {
+    lat: 34.03789812960242,
+    lon: -118.5535631839145,
+    surfline_id: 4883,
+    msw_id: 3673,
+    spitcast_id: 387,
+    name: 'Sunset Blvd',
+  },
+  {
+    lat: 33.983161270783711,
+    lon: -118.4742634651619,
+    surfline_id: 4211,
+    msw_id: 2611,
+    spitcast_id: 204,
+    name: 'Venice Breakwater',
+  },
+  {
+    lat: 33.89859255329511,
+    lon: -118.4208669620573,
+    surfline_id: 4900,
+    msw_id: 2677,
+    spitcast_id: 402,
+    name: 'El Porto',
+  },
+  {
+    lat: 33.87963506431794,
+    lon: -118.4110175106966,
+    surfline_id: 4901,
+    msw_id: 281,
+    spitcast_id: 203,
+    name: 'Manhattan Beach',
+  },
+  {
+    lat: 33.87201368173932,
+    lon: -118.4075151286262,
+    surfline_id: 4902,
+    msw_id: 2607,
+    spitcast_id: 202,
+    name: 'Hermosa Beach',
+  },
+  {
+    lat: 33.85044519861206,
+    lon: -118.4004837749971,
+    surfline_id: 4912,
+    msw_id: 4208,
+    name: 'Redondo Breakwall',
+  },
+  {
+    lat: 33.80930828074364,
+    lon: -118.3933198342649,
+    surfline_id: 4910,
+    msw_id: 2604,
+    spitcast_id: 200,
+    name: 'Torrance Beach/Haggertys',
+  },
+  {
+    lat: 33.79294656286144,
+    lon: -118.4070316699825,
+    surfline_id: 4936,
+    msw_id: 4207,
+    name: 'Palos Verdes Cove',
+  },
+  {
+    lat: 33.77209986806257,
+    lon: -118.4268428476672,
+    surfline_id: 4935,
+    msw_id: 283,
+    name: 'Lunada Bay',
+  },
+  {
+    lat: 33.737719028593382,
+    lon: -118.1078747535007,
+    surfline_id: 4217,
+    msw_id: 285,
+    spitcast_id: 222,
+    name: 'Seal Beach Pier'
+  },
+  {
+    lat: 33.736816,
+    lon: -118.103408,
+    surfline_id: 4865,
+    msw_id: 2599,
+    name: '13th St Seal Beach'
+  },
+  {
+    lat: 33.728485804642823,
+    lon: -118.0908085660303,
+    surfline_id: 4867,
+    msw_id: 2594,
+    spitcast_id: 602,
+    name: 'Surfside Jetty'
+  },
+  {
+    lat: 33.72315771853458,
+    lon: -118.0802261736895,
+    surfline_id: 4219,
+    spitcast_id: 603,
+    name: 'Anderson St'
+  },
+  {
+    lat: 33.685392159304449,
+    lon: -118.0403658241339,
+    surfline_id: 103_685,
+    msw_id: 3797,
+    spitcast_id: 604,
+    name: 'Bolsa Chica'
+  },
+  {
+    lat: 33.666156205815213,
+    lon: -118.01901000164,
+    surfline_id: 4870,
+    msw_id: 4039,
+    spitcast_id: 220,
+    name: 'Goldenwest'
+  },
+  {
+    lat: 33.663945281265853,
+    lon: -118.0140490339561,
+    surfline_id: 4871,
+    spitcast_id: 605,
+    name: '17th St Huntington'
+  },
+  {
+    lat: 33.655213981592752,
+    lon: -118.0050433408805,
+    surfline_id: 4223,
+    msw_id: 286,
+    spitcast_id: 221,
+    name: 'Huntington Pier'
+  },
+  {
+    lat: 33.648516554087408,
+    lon: -117.9932214212635,
+    surfline_id: 4875,
+    spitcast_id: 643,
+    name: 'Huntington State Beach'
+  },
+  {
+    lat: 33.628085,
+    lon: -117.959168,
+    surfline_id: 103_681,
+    msw_id: 2577,
+    name: 'Santa Ana River Jetties'
+  },
+  {
+    lat: 33.62290558073348,
+    lon: -117.9487673760861,
+    surfline_id: 43_103,
+    msw_id: 665,
+    spitcast_id: 219,
+    name: '56th St Newport Beach'
+  },
+  {
+    lat: 33.617227592487389,
+    lon: -117.93881142211259,
+    surfline_id: 4225,
+    spitcast_id: 607,
+    name: '40th Street'
+  },
+  {
+    lat: 33.615319034584573,
+    lon: -117.9369576533262,
+    surfline_id: 4226,
+    msw_id: 4683,
+    spitcast_id: 608,
+    name: '36th Street'
+  },
+  {
+    lat: 33.610958311825421,
+    lon: -117.933626701894,
+    surfline_id: 53_412,
+    msw_id: 2575,
+    spitcast_id: 651,
+    name: 'Blackies'
+  },
+  {
+    lat: 33.606646920042472,
+    lon: -117.9307384511544,
+    surfline_id: 4227,
+    spitcast_id: 609,
+    name: 'Newport Pier'
+  },
+  {
+    lat: 33.593389938669887,
+    lon: -117.88234297040751,
+    surfline_id: 4232,
+    msw_id: 287,
+    spitcast_id: 217,
+    name: 'The Wedge'
+  },
+  {
+    lat: 33.592468,
+    lon: -117.875881,
+    surfline_id: 4879,
+    msw_id: 2579,
+    name: 'Corona Del Mar'
+  },
+  {
+    lat: 33.474665045147823,
+    lon: -117.7233023880989,
+    surfline_id: 4233,
+    msw_id: 289,
+    spitcast_id: 214,
+    name: 'Salt Creek'
+  },
+  {
+    lat: 33.466191,
+    lon: -117.716886,
+    surfline_id: 4849,
+    msw_id: 4040,
+    name: 'Strands'
+  },
+  {
+    lat: 33.460330858205381,
+    lon: -117.6886485710755,
+    surfline_id: 4848,
+    msw_id: 2588,
+    spitcast_id: 213,
+    name: 'Doheny'
+  },
+  {
+    lat: 33.419731942886493,
+    lon: -117.6212676741133,
+    msw_id: 290,
+    spitcast_id: 212,
+    name: 'San Clemente Pier'
+  },
+  {
+    lat: 33.415965582403473,
+    lon: -117.618142936175,
+    surfline_id: 4235,
+    msw_id: 2570,
+    spitcast_id: 211,
+    name: 'T Street'
+  },
+  {
+    lat: 33.402979968143072,
+    lon: -117.6061996160215,
+    surfline_id: 4843,
+    spitcast_id: 392,
+    name: 'San Clemente State Beach'
+  },
+  {
+    lat: 33.390612992576763,
+    lon: -117.5987721491924,
+    surfline_id: 4737,
+    spitcast_id: 209,
+    name: 'Cottons'
+  },
+  {
+    lat: 33.384476301377603,
+    lon: -117.59445112402619,
+    surfline_id: 4738,
+    spitcast_id: 623,
+    name: 'Upper Trestles'
+  },
+  {
+    lat: 33.382116955073258,
+    lon: -117.5888397460814,
+    surfline_id: 4740,
+    msw_id: 291,
+    spitcast_id: 208,
+    name: 'Lower Trestles'
+  },
+  {
+    lat: 33.380114554751792,
+    lon: -117.5790343398904,
+    surfline_id: 4741,
+    spitcast_id: 625,
+    name: 'Church'
+  },
+  {
+    lat: 33.373605466203927,
+    lon: -117.56912919937839,
+    surfline_id: 4237,
+    msw_id: 4192,
+    spitcast_id: 239,
+    name: 'San Onofre'
+  },
+  {
+    lat: 33.343654,
+    lon: -117.519084,
+    surfline_id: 4736,
+    msw_id: 2600,
+    name: 'Trails'
+  },
+  {
+    lat: 33.204228527589997,
+    lon: -117.3959770213895,
+    surfline_id: 4238,
+    spitcast_id: 238,
+    name: 'Oceanside Harbor'
+  },
+  {
+    lat: 33.193387046160893,
+    lon: -117.3871878580306,
+    surfline_id: 68_366,
+    msw_id: 664,
+    spitcast_id: 594,
+    name: 'Oceanside Pier'
+  },
+  {
+    lat: 33.147320395176962,
+    lon: -117.3467966641187,
+    surfline_id: 4242,
+    msw_id: 292,
+    spitcast_id: 237,
+    name: 'Tamarack'
+  },
+  {
+    lat: 33.128762503805198,
+    lon: -117.3361948822189,
+    surfline_id: 4775,
+    spitcast_id: 597,
+    name: 'Terra Mar'
+  },
+  {
+    lat: 33.087034665281351,
+    lon: -117.314238172042,
+    surfline_id: 4773,
+    msw_id: 1149,
+    spitcast_id: 236,
+    name: 'Ponto'
+  },
+  {
+    lat: 33.075484467395668,
+    lon: -117.310721142163,
+    surfline_id: 4771,
+    msw_id: 4564,
+    spitcast_id: 400,
+    name: 'Grandview'
+  },
+  {
+    lat: 33.063570212034683,
+    lon: -117.30555007900939,
+    surfline_id: 4772,
+    msw_id: 4562,
+    spitcast_id: 235,
+    name: 'Beacons'
+  },
+  {
+    lat: 33.063570212034683,
+    lon: -117.30555007900939,
+    surfline_id: 4772,
+    msw_id: 4562,
+    spitcast_id: 235,
+    name: 'Beacons'
+  },
+  {
+    lat: 33.045442274103927,
+    lon: -117.2982038691907,
+    surfline_id: 4792,
+    msw_id: 4563,
+    spitcast_id: 401,
+    name: 'D Street'
+  },
+  {
+    lat: 33.034422931013466,
+    lon: -117.2957502535422,
+    surfline_id: 4789,
+    msw_id: 293,
+    spitcast_id: 234,
+    name: 'Swamis'
+  },
+  {
+    lat: 33.01541991675105,
+    lon: -117.283273919829,
+    surfline_id: 4786,
+    msw_id: 4663,
+    spitcast_id: 232,
+    name: 'Cardiff Reef'
+  },
+  {
+    lat: 33.001619,
+    lon: -117.280247,
+    surfline_id: 4787,
+    msw_id: 294,
+    name: 'Seaside Reef'
+  },
+  {
+    lat: 32.975133,
+    lon: -117.271344,
+    surfline_id: 4785,
+    msw_id: 4663,
+    name: 'Del Mar Rivermouth'
+  },
+  {
+    lat: 32.958662325867159,
+    lon: -117.2691753574579,
+    surfline_id: 4783,
+    msw_id: 3707,
+    spitcast_id: 230,
+    name: '15th Street Del Mar'
+  },
+  {
+    lat: 32.93310208353217,
+    lon: -117.2617407404518,
+    surfline_id: 107_951,
+    spitcast_id: 754,
+    name: 'Torrey Pines State Beach'
+  },
+  {
+    lat: 32.888727761985209,
+    lon: -117.25747793279859,
+    surfline_id: 4245,
+    msw_id: 294,
+    spitcast_id: 229,
+    name: 'Blacks'
+  },
+  {
+    lat: 32.866598509332697,
+    lon: -117.2562736520856,
+    surfline_id: 4246,
+    msw_id: 296,
+    spitcast_id: 228,
+    name: 'Scripps Pier'
+  },
+  {
+    lat: 32.838504,
+    lon: -117.283514,
+    surfline_id: 4247,
+    msw_id: 4210,
+    name: 'Horseshoe'
+  },
+  {
+    lat: 32.829665321372083,
+    lon: -117.28204353957889,
+    surfline_id: 4248,
+    msw_id: 4214,
+    spitcast_id: 227,
+    name: 'Windansea'
+  },
+  {
+    lat: 32.813424049908512,
+    lon: -117.2738442945035,
+    surfline_id: 4249,
+    msw_id: 4209,
+    spitcast_id: 398,
+    name: 'Bird Rock'
+  },
+  {
+    lat: 32.806945917515272,
+    lon: -117.2659989723968,
+    surfline_id: 4804,
+    spitcast_id: 399,
+    name: 'Tourmaline'
+  },
+  {
+    lat: 32.797029505435518,
+    lon: -117.25960295034579,
+    surfline_id: 4250,
+    msw_id: 663,
+    spitcast_id: 226,
+    name: 'Pacific Beach'
+  },
+  {
+    lat: 32.777929007486037,
+    lon: -117.2543264821912,
+    surfline_id: 4252,
+    msw_id: 297,
+    spitcast_id: 397,
+    name: 'Mission Beach'
+  },
+  {
+    lat: 32.749151851964093,
+    lon: -117.25534188491091,
+    surfline_id: 4253,
+    msw_id: 4212,
+    spitcast_id: 225,
+    name: 'Ocean Beach Pier'
+  },
+  {
+    lat: 32.718998904718177,
+    lon: -117.2571632713268,
+    surfline_id: 4254,
+    msw_id: 4211,
+    spitcast_id: 224,
+    name: 'Sunset Cliffs'
+  },
+  {
+    lat: 32.577928810608,
+    lon: -117.1346007967761,
+    surfline_id: 4256,
+    msw_id: 3745,
+    spitcast_id: 223,
+    name: 'Imperial Beach'
+  },
+  {
+    lat: 37.768256511796238,
+    lon: -122.51347658831379,
+    surfline_id: 4127,
+    msw_id: 255,
+    spitcast_id: 117,
+    name: 'Ocean Beach',
+  },
+  {
+    lat: 36.954087622045307,
+    lon: -121.9716900628907,
+    surfline_id: 4190,
+    msw_id: 644,
+    spitcast_id: 1,
+    name: 'Pleasure Point',
+  },
+]
+
+spots.each do |raw|
+  spot             = Spot.find_or_initialize_by(name: raw[:name])
+  spot.surfline_id = raw[:surfline_id]
+  spot.msw_id      = raw[:msw_id]
+  spot.spitcast_id = raw[:spitcast_id]
+  spot.lat         = raw[:lat]
+  spot.lon         = raw[:lon]
+  spot.save!
+end
