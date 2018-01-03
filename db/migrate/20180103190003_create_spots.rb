73bcefdabddb350904fafa4edffb29195606035f
Hugh Morrison <hmorri32@gmail.com>
2018-01-03 12:01:09 -0700

    create spots migration

---
+++ db/migrate/20180103190003_create_spots.rb
@@ -0,0 +1,14 @@
+class CreateSpots < ActiveRecord::Migration[5.1]
+  def change
+    create_table :spots do |t|
+      t.string  :name
+      t.float   :lat
+      t.float   :lon
+      t.integer :surfline_id
+      t.integer :msw_id
+      t.integer :spitcast_id
+
+      t.timestamps
+    end
+  end
+end
