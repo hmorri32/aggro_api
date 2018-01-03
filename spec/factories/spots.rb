caacc47b30132fa89c7c8a190abba7500e1b0e2e
Hugh Morrison <hmorri32@gmail.com>
2018-01-03 16:47:22 -0700

    deletes pseudocode in spots factory

--- spec/factories/spots.rb
+++ spec/factories/spots.rb
@@ -8,10 +8,3 @@ FactoryBot.define do
     spitcast_id 191
   end
 end
-
-# t.string   "name"
-# t.float    "lat"
-# t.float    "lon"
-# t.integer  "surfline_id"
-# t.integer  "msw_id"
-# t.integer  "spitcast_id"
