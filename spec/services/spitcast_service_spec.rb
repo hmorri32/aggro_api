a52facce138de2327ea4080ac9c778b133636926
Hugh Morrison <hmorri32@gmail.com>
2018-01-03 17:48:41 -0700

    adds instance methods to spec

--- spec/services/spitcast_service_spec.rb
+++ spec/services/spitcast_service_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe SpitCastService do
     end
   end
 
-  describe "class methods" do
-    describe ".weekly_forecast" do
+  describe "instance methods" do
+    describe "weekly_forecast" do
       it "returns a 7 day forecast" do
         forecast = spitcast.weekly_forecast
         expect(forecast.class).to eq Array
@@ -18,25 +18,27 @@ RSpec.describe SpitCastService do
       end
     end
 
-    describe ".daily_forecast" do
+    describe "daily_forecast" do
       it "returns a daily forecast" do
         forecast = spitcast.daily_forecast
         expect(forecast.class).to eq Array
         expect(forecast.length).to eq 25
       end
     end
+  end
 
-    describe ".daily_san_diego_tides" do
-      it "returns a tides for san diego county" do
-        forecast = spitcast.daily_san_diego_tides
+  describe "class methods" do
+    describe "daily_san_diego_tides" do
+      it "returns daily tides for san diego county" do
+        forecast = SpitCastService.daily_san_diego_tides
         expect(forecast.class).to eq Array
         expect(forecast.length).to eq 25
       end
     end
 
-    describe ".weekly_san_diego_tides" do
-      it "returns a tides for san diego county" do
-        forecast = spitcast.weekly_san_diego_tides
+    describe "weekly_san_diego_tides" do
+      it "returns weekly tides for san diego county" do
+        forecast = SpitCastService.weekly_san_diego_tides
         expect(forecast.class).to eq Array
         expect(forecast.length).to eq 169
       end
