f24383ca49680f148ee60a4d99fb49228dc2b229
Hugh Morrison <hmorri32@gmail.com>
2018-01-03 18:02:22 -0700

    spec out happy sad path spitcast forecast controller

--- spec/requests/api/v1/spitcast/forecast_spec.rb
+++ spec/requests/api/v1/spitcast/forecast_spec.rb
@@ -17,16 +17,16 @@ RSpec.describe "api/v1/spitcast/", type: :request do
         expect(response).to have_http_status(200)
       end
     end
-  end
 
-  context "given an invalid resource" do
-    before { get "/api/v1/spitcast/daily/22222" }
-    it "returns an error message" do
-      expect(response.body).to match(/route not found/)
-    end
+    context "given an invalid resource" do
+      before { get "/api/v1/spitcast/daily/22222" }
+      it "returns an error message" do
+        expect(response.body).to match(/route not found/)
+      end
 
-    it "returns http status code 404" do
-      expect(response).to have_http_status(404)
+      it "returns http status code 404" do
+        expect(response).to have_http_status(404)
+      end
     end
   end
 end
